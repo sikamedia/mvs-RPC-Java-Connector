b46e626683999738e55cb2fd38b4b3c916efbf9a
sikamedia <simonwantao@yahoo.com>
2017-09-14 12:28:18 +0200

    1) update README file.  2) correct the callAPIMethodAsynchronous method implementation, and make call Asynchronously pass.

--- README.md
+++ README.md
@@ -4,8 +4,32 @@
 ## Introduction
 
 
-This project (Metaverse RPC java connector)  is a java library that is intended to provide a wrapper around JSON-RPC provided by Metaverse (mvs.live).
+This project (Metaverse RPC java connector) is a java library that is intended to provide a wrapper around JSON-RPC provided by Metaverse [MVS](mvs.live).
 
 It is the extension of Litecoin-Bitcoin-RPC-Java-Connector using [GitHub Pages](https://github.com/nitinsurana/Litecoin-Bitcoin-RPC-Java-Connector).
 
+[MVS](http://mvs.live/)  JSON-RPC calls for [Metaverse API Call List](https://github.com/mvs-org/metaverse/wiki/Metaverse-API-Call-List).
 
+This project assumes that the user who has a preliminary knowledge about Metaverse and know how to start RPC server.
+
+## How to use
+
+######  Test Envrionment Setup
+* Start Metaverse Server (daemon).
+* Go to [test folder](https://github.com/mvshub/mvs-RPC-Java-Connector/tree/master/src/test/java/com/viewfin/metaverse/rpcconnector),
+copy config.yml.template to config.yml, and config the username and password. Strongly recommend to use https in production.
+```
+metaverse:
+  host: 127.0.0.1
+  username: xxxxxx
+  password: xxxxxx
+  port: 8820
+  protocol: http
+``` 
+* Sample usage which can be refer to [MetaverseCreateAccountTest.java](https://github.com/mvshub/mvs-RPC-Java-Connector/blob/master/src/test/java/com/viewfin/metaverse/rpcconnector/MetaverseCreateAccountTest.java).
+There are two call methods callAPIMethod and callAPIMethodAsynchronous in [CryptoCurrencyRPC.java](https://github.com/mvshub/mvs-RPC-Java-Connector/blob/master/src/main/java/com/viewfin/metaverse/rpcconnector/CryptoCurrencyRPC.java),
+which can be acted as a synchronous/asynchronous rpc client call to Metaverse RPC server.
+
+# Todo
+
+Add more call methods.
--- src/main/java/com/viewfin/metaverse/rpcconnector/MVSCryptoCurrencyRPC.java
+++ src/main/java/com/viewfin/metaverse/rpcconnector/MVSCryptoCurrencyRPC.java
@@ -1,19 +1,14 @@
 package com.viewfin.metaverse.rpcconnector;
 
-import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 import com.viewfin.metaverse.rpcconnector.exception.AuthenticationException;
-import com.viewfin.metaverse.rpcconnector.exception.CallApiCryptoCurrencyRpcException;
 import com.viewfin.metaverse.rpcconnector.exception.CryptoCurrencyRpcException;
 import com.viewfin.metaverse.rpcconnector.exception.CryptoCurrencyRpcExceptionHandler;
 import org.apache.log4j.Logger;
-import org.asynchttpclient.*;
 
 import java.math.BigDecimal;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class MVSCryptoCurrencyRPC extends CryptoCurrencyRPC {
 
@@ -48,62 +43,6 @@ public class MVSCryptoCurrencyRPC extends CryptoCurrencyRPC {
         }
 
     }*/
-
-    public JsonObject callAPIMethodAsynchronous(APICalls callMethod, Object... params) throws CallApiCryptoCurrencyRpcException {
-
-        try {
-
-            AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
-            JSONRequestBody body = new JSONRequestBody();
-            body.setMethod(callMethod.toString());
-            if (params != null && params.length > 0) {
-                body.setParams(params);
-            }
-
-
-            LOG.info(this.getPathToBaseUrl());
-
-            Request request = asyncHttpClient.preparePost(this.getPathToBaseUrl()).
-                    setHeader("Content-Type","application/json").
-                    setBody(body.toString()).build();
-
-
-
-
-            ListenableFuture<JsonObject> f = asyncHttpClient.executeRequest(request,
-                    new AsyncCompletionHandler<JsonObject>() {
-
-                        @Override
-                        public JsonObject onCompleted(Response response) throws Exception {
-                            JsonObject jsonObj = new JsonParser().parse(response.getResponseBody()).getAsJsonObject();
-                            return jsonObj;
-                        }
-
-                        @Override
-                        public void onThrowable(Throwable t){
-                            // Something wrong happened.
-                        }
-                    });
-
-            JsonObject jsonObject = null;
-
-            if (f != null) {
-                try {
-                    jsonObject = f.get();
-                } catch (InterruptedException ex) {
-                    throw new CallApiCryptoCurrencyRpcException(ex.getMessage());
-                } catch (ExecutionException ex) {
-                    throw new CallApiCryptoCurrencyRpcException(ex.getMessage());
-                }
-            }
-
-            asyncHttpClient.close();
-            return jsonObject;
-        } catch (Exception e) {
-            throw new CallApiCryptoCurrencyRpcException(e.getMessage());
-        }
-
-    }
 
     public BigDecimal getBalance() throws CryptoCurrencyRpcException {
         // JsonObject jsonObj = this.callAPIMethod(APICalls.GET_BALANCE, rpcUser, rpcPassword);
